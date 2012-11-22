0dd4ea14dbeb662990fa9b353872a977a8a62616
Tobias Crawley <tcrawley@gmail.com>
2012-11-21 22:31:29 -0500

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynapath "0.2.0"
+(defproject dynapath "0.3.0-SNAPSHOT"
   :description "An abstraction for modifiable/readable class loaders."
   :url "https://github.com/tobias/dynapath"
   :license {:name "Eclipse Public License"
