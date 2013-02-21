49135207dca98aa6dd0f862f7fa44c1805ae7a06
Toby Crawley <toby@tcrawley.org>
2013-02-21 14:55:06 -0500

    Implement :can-read? for ExtClassLoader as well.

--- src/dynapath/defaults.clj
+++ src/dynapath/defaults.clj
@@ -4,6 +4,11 @@
   (:import clojure.lang.DynamicClassLoader
            (java.net URL URLClassLoader)))
 
+(defmacro when-resolves
+  [sym & body]
+  (when (resolve sym)
+    `(do ~@body)))
+
 (let [base-url-classloader (assoc base-readable-addable-classpath
                              :classpath-urls #(seq (.getURLs %)))]
   (extend URLClassLoader
@@ -19,14 +24,14 @@
     DynamicClasspath
     (assoc base-url-classloader
       :add-classpath-url (fn [cl url]
-                           (.addURL cl url)))))
+                           (.addURL cl url))))
 
-(defmacro when-resolves
-  [sym & body]
-  (when (resolve sym)
-    `(do ~@body)))
+  (when-resolves sun.misc.Launcher
+   (extend sun.misc.Launcher$ExtClassLoader
+     DynamicClasspath
+     (assoc base-url-classloader
+       :can-add? (constantly false)))))
 
-(when-resolves sun.misc.Launcher
-   (extend-type sun.misc.Launcher$ExtClassLoader
-   DynamicClasspath
-   (can-add? [_] false)))
+
+
+
