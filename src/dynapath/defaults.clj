38fae3fd7ffeeff3d0ec435c02f5f4a36f0d46e9
Toby Crawley <toby@tcrawley.org>
2016-12-09 10:20:04 -0500

    Work properly when AOT'd
    We were resolving sun.misc.Launcher at compile-time instead of run-time,
    which caused dynapath to fail under Java 9 if AOT'd with a Java < 9.
    
    This also guards against applying the default extensions more than once.

--- src/dynapath/defaults.clj
+++ src/dynapath/defaults.clj
@@ -6,14 +6,16 @@
 
 (defmacro when-resolves
   [sym & body]
-  (when (try
-          (resolve sym)
-          (catch Exception _))
-    `(do ~@body)))
+  `(when (try
+           (resolve ~sym)
+           (catch Exception _#))
+     (eval '(do ~@body))))
 
-(let [base-url-classloader (assoc base-readable-addable-classpath
-                             :classpath-urls #(seq
-                                               (.getURLs ^URLClassLoader %)))]
+(def ^:private base-url-classloader
+  (assoc base-readable-addable-classpath
+    :classpath-urls #(seq (.getURLs ^URLClassLoader %))))
+
+(when-not (extends? DynamicClasspath URLClassLoader)
   (extend URLClassLoader
     DynamicClasspath
     (assoc base-url-classloader
@@ -31,12 +33,9 @@
 
   ;; on < java 9, the boot classloader is a URLClassLoader, but
   ;; modifying it can have dire consequences
-  (when-resolves sun.misc.Launcher
-   (extend sun.misc.Launcher$ExtClassLoader
-     DynamicClasspath
-     (assoc base-url-classloader
-       :can-add? (constantly false)))))
-
-
-
+  (when-resolves 'sun.misc.Launcher$ExtClassLoader
+    (extend sun.misc.Launcher$ExtClassLoader
+      DynamicClasspath
+      (assoc base-url-classloader
+        :can-add? (constantly false)))))
 
