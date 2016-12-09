c0c82c9a36d0efadf3e19899cdee7e1a1f2ea9b2
Toby Crawley <toby@tcrawley.org>
2016-12-09 14:26:22 -0500

    Version 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.tcrawley/dynapath "0.2.5-SNAPSHOT"
+(defproject org.tcrawley/dynapath "0.2.5"
   :description "An abstraction for modifiable/readable class loaders."
   :url "https://github.com/tobias/dynapath"
   :license {:name "Eclipse Public License"
