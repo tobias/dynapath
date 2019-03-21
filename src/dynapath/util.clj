0f446577703e1c06b38f191fc8d90e61de491597
Vlad Bokov <razum2um@mail.ru>
2019-03-21 15:00:19 +0700

    Fix empty all-classpath-urls on Java 9+

--- src/dynapath/util.clj
+++ src/dynapath/util.clj
@@ -1,7 +1,8 @@
 (ns dynapath.util
   "Abstracts the getURLs and addURL functionality of URLClassLoader to a protocol."
   (:require [dynapath.dynamic-classpath :as dc]
-            dynapath.defaults)) ;; trigger the default implementations
+            dynapath.defaults) ;; trigger the default implementations
+  (:import [java.io File]))
 
 (defn addable-classpath?
   "Returns true if the given ClassLoader provides add-claspath-url."
@@ -21,11 +22,21 @@
   (if (readable-classpath? cl)
     (dc/classpath-urls cl)))
 
+(defn- system-classpath
+  "Starting with Java 9, the default class loader is no longer an
+  instance of URLClassLoader, so `classpath` returned an empty sequence.
+  See more in clojure/java.classpath"
+  []
+  (map #(-> (File. ^String %) .toURI .toURL)
+       (.split (System/getProperty "java.class.path")
+               (System/getProperty "path.separator"))))
+
 (defn all-classpath-urls
   "Walks up the parentage chain for a ClassLoader, concatenating any URLs it retrieves.
 If no ClassLoader is provided, RT/baseLoader is assumed."
   ([]
-     (all-classpath-urls (clojure.lang.RT/baseLoader)))
+   (or (seq (all-classpath-urls (clojure.lang.RT/baseLoader)))
+       (system-classpath)))
   ([cl]
      (->> (iterate #(.getParent ^ClassLoader %) cl)
           (take-while identity)
